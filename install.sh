4d2ca8b81739803e6ab0efa84774045ebe1dd921
bmcorser <bmcorser@gmail.com>
2015-09-26 11:12:34 +0100

    Going for travis

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
-if [ -f ngrok ]; then
+#!/bin/bash
+if [ -f test/system/ngrok ]; then
     echo "ngrok is present"
 else
     echo "installing ngrok"
+    pushd test/system
     wget https://dl.ngrok.com/ngrok_2.0.19_linux_amd64.zip -O ngrok.zip
     unzip ngrok.zip
     rm -v ngrok.zip
+    popd
 fi
+pip install -r requirements.txt
+pip install -r test-requirements.txt
