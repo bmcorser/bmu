6c6c14cad09e704bee367859ea7cdacb0e482d19
bmcorser <bmcorser@gmail.com>
2015-09-29 16:08:25 +0100

    Cool echo server with a limited life

---
+++ test/system/echo_server.py
@@ -0,0 +1,54 @@
+import json
+import tempfile
+from flask import Flask, request
+from plumbum import cli
+import logging
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+app = Flask(__name__)
+
+CURRENT = 0
+
+
+class ServeN(cli.Application):
+    'Serve a number of requests then harikiri'
+    _port = 9000
+    _n = 0
+
+    @cli.switch(['-p', '--port'], int)
+    def server_port(self, port):
+        self._port = port
+
+    @cli.switch(['-n', '--requests'], int)
+    def requests(self, n):
+        self._n = n
+
+    def main(self):
+        file_list = [
+            tempfile.NamedTemporaryFile(delete=False) for n in range(self._n)
+        ]
+
+        @app.route('/', methods=['POST'])
+        def everything():
+            global CURRENT
+            try:
+                current_file = file_list[CURRENT]
+            except IndexError:
+                if self._n == 0:
+                    current_file = tempfile.NamedTemporaryFile(delete=False)
+                    file_list.append(current_file)
+            current_file.write(request.data)
+            current_file.flush()
+            current_file.close()
+            CURRENT += 1
+            if CURRENT == (self._n):
+                print(json.dumps([f.name for f in file_list]))
+                request.environ.get('werkzeug.server.shutdown')()
+            return 'Ok'
+
+        app.run(port=self._port)
+
+if __name__ == '__main__':
+    ServeN.run()
